65c2cee463ade3e952688c98a5126db631248725
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2023-10-04 21:33:45 -0400

    Navigate within grid view

--- source/main.lua
+++ source/main.lua
@@ -1,6 +1,7 @@
 import "CoreLibs/graphics"
 import "CoreLibs/timer"
 import "CoreLibs/ui"
+import "CoreLibs/sprites"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
@@ -10,6 +11,11 @@ local gridview = pd.ui.gridview.new(32, 32)
 gridview:setNumberOfColumns(8)
 gridview:setNumberOfRows(6)
 gridview:setCellPadding(2, 2, 2, 2)
+
+local gridviewSprite = gfx.sprite.new()
+gridviewSprite:setCenter(0, 0)
+gridviewSprite:moveTo(100, 70)
+gridviewSprite:add()
 
 function gridview:drawCell(section, row, column, selected, x, y, width, height)
 if selected then
@@ -25,6 +31,23 @@ if selected then
 end
 
 function pd.update()
-	gridview:drawInRect(100, 70, 200, 100)
+	if pd.buttonJustPressed(pd.kButtonUp) then
+		gridview:selectPreviousRow(true)
+	elseif pd.buttonJustPressed(pd.kButtonDown) then
+		gridview:selectNextRow(true)
+	elseif (pd.buttonJustPressed(pd.kButtonLeft)) then
+		gridview:selectPreviousColumn(false)
+	elseif (pd.buttonJustPressed(pd.kButtonRight)) then
+		gridview:selectNextColumn(false)
+	end
+
+	local gridviewImage = gfx.image.new(200, 100)
+	gfx.pushContext(gridviewImage)
+		gridview:drawInRect(0, 0, 200, 100)
+	gfx.popContext()
+	gridviewSprite:setImage(gridviewImage)
+
+	gfx.sprite.update()
+
 	pd.timer.updateTimers()
 end
