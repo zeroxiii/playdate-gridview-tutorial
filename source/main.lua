f1c52b9ee5ef9ca1c9ba425022e88df076e69b9a
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2023-10-04 21:21:46 -0400

    Draw circles instead of squares

--- source/main.lua
+++ source/main.lua
@@ -9,6 +9,15 @@ local gridview = pd.ui.gridview.new(32, 32)
 
 gridview:setNumberOfColumns(8)
 gridview:setNumberOfRows(6)
+gridview:setCellPadding(2, 2, 2, 2)
+
+function gridview:drawCell(section, row, column, selected, x, y, width, height)
+if selected then
+		gfx.fillCircleInRect(x, y, width, height)
+	else
+		gfx.drawCircleInRect(x, y, width, height)
+	end
+end
 
 function pd.update()
 	gridview:drawInRect(100, 70, 200, 100)
