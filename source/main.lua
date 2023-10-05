eab19b5f2f8f66f49dfa71666ae7b9384a65ee65
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2023-10-04 21:26:28 -0400

    Added text to circles

--- source/main.lua
+++ source/main.lua
@@ -14,9 +14,14 @@ gridview:setCellPadding(2, 2, 2, 2)
 function gridview:drawCell(section, row, column, selected, x, y, width, height)
 if selected then
 		gfx.fillCircleInRect(x, y, width, height)
+		gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
 	else
 		gfx.drawCircleInRect(x, y, width, height)
+		gfx.setImageDrawMode(gfx.kDrawModeCopy)
 	end
+	local cellText = tostring(row) .. "-" .. tostring(column)
+	local fontHeight = gfx.getSystemFont():getHeight()
+	gfx.drawTextInRect(cellText, x, y + (height/2 - fontHeight/2) + 2, width, height, nil, nil, kTextAlignment.center)
 end
 
 function pd.update()
